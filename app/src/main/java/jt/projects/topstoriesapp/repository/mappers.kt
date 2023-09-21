47946b5deb1f61a295cd844b9a4e3bb9720c9931
VladJT <onlyrus@hotmail.com>
2023-09-21 09:33:17 +0300

    ref mappers

--- app/src/main/java/jt/projects/topstoriesapp/repository/mappers.kt
+++ app/src/main/java/jt/projects/topstoriesapp/repository/mappers.kt
@@ -4,16 +4,14 @@ package jt.projects.topstoriesapp.repository
 import jt.projects.topstoriesapp.model.Story
 import jt.projects.topstoriesapp.repository.dto.StoryDTO
 
+
 fun StoryDTO.toStoryList(): List<Story> {
-    val result = mutableListOf<Story>()
-    this.results.forEach { data ->
-        result.add(
-            Story(
-                description = data.multimedia[0].caption ?: "",
-                title = data.title ?: "no title",
-                imageUrl = data.multimedia[0].url ?: ""
-            )
+    return this.results.map { data ->
+        Story(
+            description = data.multimedia[0].caption ?: "",
+            title = data.title ?: "no title",
+            imageUrl = data.multimedia[0].url ?: ""
         )
     }
-    return result
+
 }
